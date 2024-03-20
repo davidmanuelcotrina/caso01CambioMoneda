1db9d89d8a9192b57b469f3545f64e4b8a54d260
davidmanuelcotrina <davidmanuelcotrina@hotmail.com>
2024-03-20 16:26:18 -0500

    primer cambio

---
+++ src/test/java/com/testMoneda/Caso01CambioMonedaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testMoneda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Caso01CambioMonedaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
